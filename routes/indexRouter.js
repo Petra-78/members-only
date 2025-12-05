77e120f6b15d3c8d260456c413668d9d0393e10f
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 20:17:34 +0100

    Import admin and delete controllers

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,10 +5,19 @@ const {
   renderMembership,
   postMembership,
   isAuthenticated,
+  renderAdmin,
+  postAdmin,
+  deleteMessage,
+  isAdmin,
 } = require("../controllers/authController");
 
 router.get("/", indexController.renderHomepage);
 router.get("/membership", isAuthenticated, renderMembership);
 router.post("/membership", isAuthenticated, postMembership);
+
+router.get("/admin", isAuthenticated, renderAdmin);
+router.post("/admin", isAuthenticated, postAdmin);
+
+router.post("/delete/:id", isAuthenticated, isAdmin, deleteMessage);
 
 module.exports = router;
