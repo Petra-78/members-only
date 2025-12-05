85347a806ecf5f8e0eccb890b179fc2cc442111d
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 18:55:28 +0100

    Add table join query for message rendering

--- models/manageUsers.js
+++ models/manageUsers.js
@@ -10,4 +10,15 @@ INSERT INTO users (
   RETURNING *;
 `;
 
-module.exports = { ADD_USER };
+const JOIN_TABLES = `
+SELECT users.first_name, users.last_name, messages.title, messages.message, messages.date
+FROM users
+JOIN messages ON users.id = messages.user_id;
+`;
+
+const ADD_MESSAGE = `
+INSERT INTO messages (title, message, date, user_id ) 
+VALUES ($1, $2, $3, $4)
+`;
+
+module.exports = { ADD_USER, JOIN_TABLES, ADD_MESSAGE };
