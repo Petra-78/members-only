06c29da013961f19a26ac746db2cd8bfea77978c
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 12:21:39 +0100

    Add signup and login controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,44 @@
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const pool = require("../db/pool");
 const { ADD_USER } = require("../models/manageUsers");
+const { validationResult } = require("express-validator");
 
 async function signUp(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("sign-up-form", { errors: errors.array() });
+  }
+
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await pool.query(ADD_USER, [req.body.username, hashedPassword]);
-    res.redirect("/");
+
+    const result = await pool.query(ADD_USER, [
+      req.body.first_name,
+      req.body.last_name,
+      req.body.username,
+      hashedPassword,
+      false,
+      false,
+    ]);
+
+    const newUser = result.rows[0];
+    console.log("NEW USER:", newUser);
+
+    req.login(newUser, (err) => {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
   } catch (err) {
     return next(err);
   }
 }
 
-module.exports = { signUp };
+async function postLogin(req, res, next) {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  });
+}
+
+module.exports = { signUp, postLogin };
