f98f1153c04f86334a366d4272df016cdee6050c
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 18:56:58 +0100

    Remove error object from get request

--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -3,7 +3,7 @@ const router = Router();
 const authController = require("../controllers/authController");
 const { signupValidator } = require("../validators/authValidator");
 
-router.get("/", (req, res) => res.render("sign-up-form", { errors: [] }));
+router.get("/", (req, res) => res.render("sign-up-form"));
 router.post("/", signupValidator, authController.signUp);
 
 module.exports = router;
