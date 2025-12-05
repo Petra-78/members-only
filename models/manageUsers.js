e7e4045d7de0df0f19d09aaf8647691784763421
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 12:24:40 +0100

    Fix insert query so it adds all input values to the table

--- models/manageUsers.js
+++ models/manageUsers.js
@@ -1,5 +1,13 @@
 const ADD_USER = `
-INSERT INTO users (username, password) VALUES ($1, $2);
+INSERT INTO users (
+  first_name,
+  last_name,
+  username,
+  password,
+  membership_status,
+  admin) 
+  VALUES ($1, $2, $3, $4, $5, $6)
+  RETURNING *;
 `;
 
 module.exports = { ADD_USER };
