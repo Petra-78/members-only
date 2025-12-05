c99908f3409d0318d7b8c8ee07b96cd846fa0cdb
Petra-78 <partos.petra.0305@gmail.com>
2025-12-05 12:27:18 +0100

    Initial commit with basic setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("dotenv").config();
+require("./config/passport");
+
+const signupRouter = require("./routes/signupRouter");
+const loginRouter = require("./routes/loginRouter");
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log(req.body, req.locals);
+  res.render("index");
+});
+
+app.use("/log-in", loginRouter);
+app.use("/sign-up", signupRouter);
+
+app.listen(process.env.PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`app listening on port ${process.env.PORT}!`);
+});
